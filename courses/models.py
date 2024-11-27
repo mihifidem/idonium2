094b553cdfb326e661b59a56f85a57a55aede36a
PauSilveira <pau.silveira12@gmail.com>
2024-11-27 11:18:25 +0100

    last_fixes_v2

--- courses/models.py
+++ courses/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class Status(models.Model):
     name = models.CharField(max_length=15)
     description = models.TextField(blank=True)
-
+    
     def __str__(self):
         return self.status
     
